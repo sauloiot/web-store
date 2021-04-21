f91b96c46e466133293ab97a0c210f37949ec7ca
sauloiot <saulo.11@hotmail.com>
2021-04-21 03:00:55 -0300

    first commit

---
+++ src/test/java/com/saulo/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulo.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
