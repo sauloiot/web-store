f3d333f0ccad5ee601889cfd22320022995a0036
sauloiot <saulo.11@hotmail.com>
2021-04-22 20:07:28 -0300

    Adiciona conexão ao banco de dados, adiciona perfil de teste, desativa segurança no perfil de teste, cria CategoriaController e adiciona o teste CategoriaControllerTest

---
+++ src/test/java/com/saulo/webstore/controllers/CategoriaControllerTest.java
@@ -0,0 +1,38 @@
+package com.saulo.webstore.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CategoriaController.class)
+@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+class CategoriaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listar() throws Exception {
+        mockMvc.perform(get("/categorias", "Rest Funcionando !"))
+                .andExpect(status().isOk());
+    }
+
+}
