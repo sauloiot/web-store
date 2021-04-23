3ac5d29703bd42572b474775a2eb25615b84224a
sauloiot <saulo.11@hotmail.com>
2021-04-23 00:09:08 -0300

    Testes save, findAll, findById e deleteById em CategoriaRepositoryTest passaram.

--- src/test/java/com/saulo/webstore/repositories/CategoriaRepositoryTest.java
+++ src/test/java/com/saulo/webstore/repositories/CategoriaRepositoryTest.java
@@ -4,8 +4,13 @@ import com.saulo.webstore.models.Categoria;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -27,5 +32,21 @@ class CategoriaRepositoryTest {
         Categoria cat = new Categoria(1, "Informatica");
         Categoria categoria = repository.save(cat);
         assertThat(categoria).isNotNull();
+    }
+    @Test
+    void testFindById(){
+        Optional<Categoria> accUser = repository.findById(1);
+        assertThat(accUser).isNotNull();
+    }
+    @Test
+    void testFindAll(){
+        List<Categoria> accUser = repository.findAll();
+        assertThat(accUser).isNotNull();
+    }
+    @Test
+    void testDeleteById(){
+        Categoria cat = new Categoria(1, "Informatica");
+        Categoria categoria = repository.save(cat);
+        repository.deleteById(categoria.getId());
     }
 }
