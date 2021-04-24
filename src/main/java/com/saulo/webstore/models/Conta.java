f4e4879bbda9a6c10b57d97f5f5cd5d50846ef3e
sauloiot <saulo.11@hotmail.com>
2021-04-24 18:46:14 -0300

    Cria o model conta e sua classe de teste com resultado Ok

---
+++ src/main/java/com/saulo/webstore/models/Conta.java
@@ -0,0 +1,98 @@
+package com.saulo.webstore.models;
+
+import com.saulo.webstore.models.enums.TipoConta;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Conta implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String senha;
+    private Integer tipoConta;
+
+    public Conta() {
+    }
+
+    public Conta(Integer id, String nome, String email, String senha, TipoConta tipoConta) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.tipoConta = tipoConta.getCod();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoConta getTipoConta() {
+        return TipoConta.toEnum(tipoConta);
+    }
+
+    public void setTipoConta(TipoConta tipoConta) {
+        this.tipoConta = tipoConta.getCod();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return id.equals(conta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", tipoConta=" + tipoConta +
+                '}';
+    }
+}
---
+++ src/test/java/com/saulo/webstore/models/ContaTest.java
@@ -0,0 +1,34 @@
+package com.saulo.webstore.models;
+
+import com.saulo.webstore.models.enums.TipoConta;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testModelCategoria() {
+        Conta conta = new Conta(1, "Administrador", "adm@hotmail.com", "0123456", TipoConta.ADMIN);
+
+        assertThat(conta)
+                .isNotNull()
+                .satisfies(u -> {
+                    assertThat(u.getId()).isEqualTo(1);
+                    assertThat(u.getNome()).isEqualTo("Administrador");
+                    assertThat(u.getEmail()).isEqualTo("adm@hotmail.com");
+                    assertThat(u.getSenha()).isEqualTo("0123456");
+                    assertThat(u.getTipoConta()).isEqualTo(TipoConta.ADMIN);
+                });
+    }
+}
