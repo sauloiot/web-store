1914cae39271ab6970e3abc75d1fc9450a1cce27
sauloiot <saulo.11@hotmail.com>
2021-04-29 04:33:40 -0300

    Pequena correção em SecurityConfig

--- src/main/java/com/saulo/webstore/config/SecurityConfig.java
+++ src/main/java/com/saulo/webstore/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/cupom/**"
     };
     private static final String[] PUBLIC_MATCHERS_POST = {
-            "/clientes/**"
+            "/contas/**"
     };
 
     @Override
